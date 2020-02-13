1ffa644530c7c9a010bdc17d31b62b1b61b8f861
Lijinghan9611 <1562321291@qq.com>
2020-02-13 17:49:44 +0800

    parseurl test

--- test_Javascript.js
+++ test_Javascript.js
@@ -1,33 +1,88 @@
-QS : https://github.com/mqyqingfeng/Blog/issues/2
-var obj = new Object();
-obj.name = 'Ka';
-console.log(obj.name)
+function parseUrl(url){
+  if(!url){ //url没有值
+    return {}
+  }
 
-function Person() {
-    console.log('222')
+  let protocol = "";
+
+  let url_temp;  //保存剩余的
+  let param_temp  //保存参数字符串
+  let url_split;  //链接切割数组
+  if(url.indexOf(":") != -1){  //获得协议
+    url_split = url.split(':')
+    protocol = url_split[0]; //取得协议
+  }
+
+  url_temp = url_split[1].replace('//', '');  //去掉链接中的双斜杠
+  param_temp = url_temp.split("?")[1];  //取到参数
+  url_temp = url_temp.split("?")[0];  //取到去掉协议非参数部分
+  console.log(protocol);
+  console.log(param_temp);
+  let host_end_index = url_temp.indexOf('/'); //取得域名结束的下标
+  let host = url_temp.substring(0, host_end_index);  //域名/IP
+  let path = url_temp.replace(host, "");  //路径
+  console.log(host);
+  console.log(path);
+  let hash = '';
+  if(param_temp.indexOf('#') != -1){    //取得hash
+    hash = param_temp.split('#')[1]
+    param_temp = param_temp.split('#')[0]
+  }
+  console.log(param_temp);
+  console.log(hash);
+
+  let query = {}  //链接参数
+
+  if(param_temp){
+
+  }
 }
 
-Person.prototype.name = 'kevin';
-var person = new Person();
-person.name = 'Kevin11';
-console.log(person.name) // Kevin
-console.log(person.__proto__ === Person.prototype)
+function parseUrl(url){
+  let protocol = url.indexOf(':') != -1? url.match(/(\S*):/)[1]:''; //取得协议
 
-console.log(Person.prototype.constructor === Person)
-console.log(Person.prototype)
-console.log(Object.getPrototypeOf(person) === Person.prototype)
+  //开始匹配域名和路径
+  let hostAndPath = url.indexOf('?') != -1? url.match(/\/\/(\S*)\?/)[1] : url.match(/\/\/(\S*)/)[1]  //域名和路径
+  let host_end_index = hostAndPath.indexOf('/');  //取得域名和路径分隔斜杠的下标
+  let host = '';
+  let path = ''
+  if(host_end_index != -1){
+    host = hostAndPath.substring(0, host_end_index);  //取得域名
+    path = hostAndPath.replace(host, "")  //取得路径
+  }
+  //结束匹配域名和路径
+  
+  let queryStr;
+  let hash;
+  if(url.indexOf('#') != -1){  //链接包含#
+    queryStr = url.indexOf('?') != -1? url.match(/\?(\S*)#/)[1] : ''  //取得参数字符串
+    hash = url.match(/#(\S*)/)[1];  //取得hash
+  }
+  else{
+    queryStr = url.indexOf('?') != -1?url.match(/\?(\S*)/)[1]: ''  //取得参数字符串
+    hash = "";  //取得hash
+  }
 
-var foo = {}
-console.log(foo.__proto__)
+  let query = {};
+  if(queryStr.trim().length != 0){  //获取参数
+    let _temp_query = queryStr.split('&');
+    let query_resultStr = "";
+    _temp_query.forEach(val => {  //循环组建参数json字符串
+      val = '"' + val.replace("=" ,'":"') + '",';
+      query_resultStr += val;
+    });
+    query_resultStr = "{" + query_resultStr.substring(0, query_resultStr.length - 1) + "}";
+    query = JSON.parse(query_resultStr);
+  }
 
-var person2 = new Person.prototype.constructor();
-console.log(person2)
+  return {
+    protocol: protocol,
+    host: host,
+    path: path,
+    query: query,
+    hash: hash
+  }
+}
 
-console.log(typeof Person.prototype.constructor)
-
-person.name= 'daisy';
-console.log(person.name)
-delete person.name;
-console.log(person.name);
-
-console.log(person.constructor.name)
+var re = parseUrl("http://www.xiyanghui.com/product/list?id=123456&sort=discount#title")
+console.log(re)
